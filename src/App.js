b8de7d4fec4b8fb4ffc24cdd52f92a15c7505b07
OzFovea <ozmenbatuhan@hotmail.com>
2023-05-29 14:28:37 +0300

    Mrb2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import Header from './Header'
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } 
+from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="app">
-      <h1></h1>
-      {/* Header */}
-      <Header/>
-      {/* Home */}
-      <Home/>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes> {/*Kullandığımız react-router-dom güncel, eski versiyonda Switch ile yapılıyordu.*/}
+          <Route path="/login" element={<Login />}/>
+          <Route path="/checkout" element={<><Header /><Checkout /></>} />
+          <Route path="/" element={<><Header /><Home /></>} /> {/*!!!!Home route'unun en altta kaldığından emin olun.!!!!*/}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './Checkout.css';
+import Subtotal from './Subtotal';
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
+
+{/*Checkout sayfamız 2 ye ayrılıyor. Left -> Orders, Right-> Complete */}
+
+function Checkout() {
+  const[{basket}, dispatch] = useStateValue();
+
+  return( 
+  <div className="checkout">
+    <div className="checkout_left"> {/*Alt satırdak src'deki fotoğraf değiştirilmeli. */}
+        <img className="checkout_ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
+
+    <div>
+        <h2 className="checkout_title">
+            Your Shopping Basket
+        </h2>
+
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating} 
+            />
+          ))}
+
+            {/*Checkout Product*/}
+            {/*Checkout Product*/}
+            {/*Checkout Product*/}
+            {/*Checkout Product*/}
+
+    </div>
+    </div>
+
+    <div className="checkout_right">
+        <Subtotal />
+    </div>
+
+  </div>
+
+  );
+}
+
+export default Checkout
