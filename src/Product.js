967ea0f4f48d5d814a7c1ef82d2c34fa403a1a1b
OzFovea <ozmenbatuhan@hotmail.com>
2023-05-29 14:22:06 +0300

    Mrb

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import './Product.css'
 import StarIcon from '@mui/icons-material/Star';
+import { useStateValue } from './StateProvider';
 
-function Product({id,title,image,price,rating}) {
+function Product({id, title, image, price, rating}) {
+  const[basket, dispatch] = useStateValue();
+
+  console.log("This is basket:", basket);
+  
+  const addToBasket = () => {
+    //dispatch the item into the data layer
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className='product'>
         <div className='product__info'>
@@ -14,12 +33,12 @@ function Product({id,title,image,price,rating}) {
                 <strong>{price}</strong>
             </p>
             <div className='product__rating'>
-              {/* burada yarım yıldız ve boş yıldız kullanıp bir düzen yapabiliriz bunların verisini gene db'den alırız  */}
+              {/* burada yarım yıldız ve boş yıldız kullanıp bir düzen yapabiliriz bunların verisini gene db'den alırız EKLEME: Rating arrayini CheckoutProduct.js kısmında da kullandık. yarım yıldız mevzusunu oraya da eklememiz gerekir. */}
             {Array(rating).fill().map((_, i) => (<p><StarIcon/></p>))}
             </div>
         </div>
         <img src={image}/>
-        <button>Add to Basket</button>
+        <button onClick={addToBasket}>Add to Basket</button>
     </div>
   )
 }
