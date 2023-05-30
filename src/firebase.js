b616ccad9fb8e9f147f150fade7de5e5c8389145
TrathoN <mucahid.akca234@gmail.com>
2023-05-30 18:49:31 +0300

    sa

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
 const firebaseConfig = {
   apiKey: "AIzaSyB2pBq8B1uNnUBafGaukgOjGAo1fJI6bu8",
   authDomain: "fitfit-project.firebaseapp.com",
@@ -8,3 +12,10 @@ const firebaseConfig = {
   appId: "1:563034144201:web:7047dfa15688eda054fc18",
   measurementId: "G-SLFK2XH8NW"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
