0e228056fbd3aaf0a4d7b56b0c48e37665bc8f7a
Jeglenea <90385007+Jeglenea@users.noreply.github.com>
2023-06-12 16:37:59 +0300

    popupModal can select by id

--- src/Home.js
+++ src/Home.js
@@ -10,144 +10,171 @@ import diet03 from "./img/guide-low-carb-foods.jpg";
 import diet04 from "./img/guide-time-restricted-eating.jpg";
 
 function Home() {
-
   const product = [
     {
-    id: "12321341",
-    title: "FB Blend - Burn Fat, Build Muscle, Tone; 35 or 55 Minutes a Day",
-    price: 11.99,
-    rating: 5,
-    image: fitness01,
-    type: "workout",
-    difficulty: 4,
-    dailyDuration: " Avg. 38 Minutes - Min. 32 Minutes - Max. 45 Minutes",
-    totalDuration: "4 Weeks",
-    bodyFocus: "Total Body",
-    trainingType: "Balance/Agility, Cardiovascular, HIIT, Pilates, Strength Training, Toning",
-    equipment: "Dumbbell"
-  },
-  {
-    id: "49538094",
-    title: "FB Low Impact Round 2 - Build Muscle & Burn Fat - 40 Minutes or Less",
-    price: 239.0,
-    rating: 4,
-    image: fitness02,
-    type: "workout",
-    difficulty: 3,
-    dailyDuration: " Avg. 34 Minutes - Min. 27 Minutes - Max. 40 Minutes",
-    totalDuration: "4 Weeks",
-    bodyFocus: "Total Body",
-    trainingType: "Balance/Agility, Cardiovascular, Low Impact, Pilates, Strength Training, Toning, Stretching/Flexibility",
-    equipment: "Dumbbell"
-  },
-  {
-    id: "4903850",
-    title: "High Protein Diet Program",
-    price: 199.99,
-    rating: 3,
-    image: diet01,
-    type: "diet"
-  },
-  {
-    id: "23445930",
-    title: "Keto Diet Program",
-    price: 98.99,
-    rating: 5,
-    image: diet02,
-    type: "diet"
-  },
-  {
-    id: "3254354345",
-    title: "Low Carbohydrate Diet Program",
-    price: 598.99,
-    rating: 4,
-    image: diet03,
-    type: "diet"
-  },
-  {
-    id: "90829332",
-    title: "Intermittent Fasting Diet",
-    price: 94.98,
-    rating: 3,
-    image: diet04,
-    type: "diet"
-
-  }
-  ]
-
+      id: "12321341",
+      title: "FB Blend - Burn Fat, Build Muscle, Tone; 35 or 55 Minutes a Day",
+      price: 11.99,
+      rating: 5,
+      image: fitness01,
+      type: "workout",
+      difficulty: 4,
+      dailyDuration: " Avg. 38 Minutes - Min. 32 Minutes - Max. 45 Minutes",
+      totalDuration: "4 Weeks",
+      bodyFocus: "Total Body",
+      trainingType:
+        "Balance/Agility, Cardiovascular, HIIT, Pilates, Strength Training, Toning",
+      equipment: "Dumbbell",
+    },
+    {
+      id: "49538094",
+      title:
+        "FB Low Impact Round 2 - Build Muscle & Burn Fat - 40 Minutes or Less",
+      price: 239.0,
+      rating: 4,
+      image: fitness02,
+      type: "workout",
+      difficulty: 3,
+      dailyDuration: " Avg. 34 Minutes - Min. 27 Minutes - Max. 40 Minutes",
+      totalDuration: "4 Weeks",
+      bodyFocus: "Total Body",
+      trainingType:
+        "Balance/Agility, Cardiovascular, Low Impact, Pilates, Strength Training, Toning, Stretching/Flexibility",
+      equipment: "Dumbbell",
+    },
+    {
+      id: "4903850",
+      title: "High Protein Diet Program",
+      price: 199.99,
+      rating: 3,
+      image: diet01,
+      type: "diet",
+    },
+    {
+      id: "23445930",
+      title: "Keto Diet Program",
+      price: 98.99,
+      rating: 5,
+      image: diet02,
+      type: "diet",
+    },
+    {
+      id: "3254354345",
+      title: "Low Carbohydrate Diet Program",
+      price: 598.99,
+      rating: 4,
+      image: diet03,
+      type: "diet",
+    },
+    {
+      id: "90829332",
+      title: "Intermittent Fasting Diet",
+      price: 94.98,
+      rating: 3,
+      image: diet04,
+      type: "diet",
+    },
+  ];
 
   const handleClick = () => {
     const modal = document.getElementById("popup");
     modal.classList.add("show");
-  }
+  };
   const hidePopup = () => {
     const modal = document.getElementById("popup");
     modal.classList.remove("show");
-
-  }
+  };
 
   const popupModal = (index) => {
-    return(
+    const selectedProduct = product.find((item) => item.id === index);
+
+    if (!selectedProduct) {
+      return null;
+    }
+    return (
       <div className="modal-popup" id="popup">
-          <div className="modal-blocker" onClick={hidePopup}></div>
-          <div class="modal-fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content" id="modal-display">
-                <div class="modal-body">
-                  <div class="column" id="main">
-                    <h1> </h1>
-                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-                    <form>
-                      <div class="form-group">
-                        <label for="exampleInputName">{product[index].id}</label>
-                        <input type="name" class="form-control" id="exampleInputName" placeholder="Name" />
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">E-mail </label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail" />
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                      </div>
-                      <button type="submit" class="btn btn-primary">Sign Up</button>
-                    </form>
+        <div className="modal-blocker" onClick={hidePopup}></div>
+        <div
+          class="modal-fade"
+          id="exampleModal"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog" role="document">
+            <div class="modal-content" id="modal-display">
+              <div class="modal-body">
+                <div class="column" id="main">
+                  <Product
+                    id={selectedProduct.id}
+                    title={selectedProduct.title}
+                    price={selectedProduct.price}
+                    rating={selectedProduct.rating}
+                    image={selectedProduct.image}
+                    type="workout"
+                    difficulty={4}
+                    dailyDuration=" Avg. 38 Minutes -
+                            Min. 32 Minutes -
+                            Max. 45 Minutes"
+                    totalDuration="4 Weeks"
+                    bodyFocus="Total Body"
+                    trainingType="Balance/Agility, Cardiovascular, HIIT, Pilates, Strength Training, Toning"
+                    equipment="Dumbbell"
+                  />
+                  <div class="moreinfo info">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Sed finibus fermentum dui, finibus varius felis placerat
+                      in. Etiam eget erat eu nulla tincidunt tincidunt. Cras
+                      porttitor faucibus arcu, vitae hendrerit mauris convallis
+                      ut. Nulla ut cursus ex. Suspendisse ultricies neque non
+                      luctus tempor.
+                    </p>
                   </div>
-                  <div>
-                    <svg width="67px" height="578px" viewBox="0 0 67 578">
-                      <title>Path</title>
-                      <desc>Created with Sketch.</desc>
-                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <path d="M11.3847656,-5.68434189e-14 C-7.44726562,36.7213542 5.14322917,126.757812 49.15625,270.109375 C70.9827986,341.199016 54.8877465,443.829224 0.87109375,578 L67,578 L67,-5.68434189e-14 L11.3847656,-5.68434189e-14 Z" id="Path" fill="#F9BC35"></path>
-                      </g>
-                    </svg>
-                  </div>
-                  <div class="column" id="secondary">
-                    <div class="sec-content">
-                      <h2>Welcome Back!</h2>
-                      <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-                      <button type="button" class="btn btn-primary">Login</button>
-                    </div>
+                </div>
+                <div>
+                  <svg width="67px" height="578px" viewBox="0 0 67 578">
+                    <title>Path</title>
+                    <desc>Created with Sketch.</desc>
+                    <g
+                      id="Page-1"
+                      stroke="none"
+                      stroke-width="1"
+                      fill="none"
+                      fill-rule="evenodd"
+                    >
+                      <path
+                        d="M11.3847656,-5.68434189e-14 C-7.44726562,36.7213542 5.14322917,126.757812 49.15625,270.109375 C70.9827986,341.199016 54.8877465,443.829224 0.87109375,578 L67,578 L67,-5.68434189e-14 L11.3847656,-5.68434189e-14 Z"
+                        id="Path"
+                        fill="#F9BC35"
+                      ></path>
+                    </g>
+                  </svg>
+                </div>
+                <div class="column" id="secondary">
+                  <div class="sec-content">
+                    <h2>Welcome Back!</h2>
+                    <h3>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </h3>
+                    <button type="button" class="btn btn-primary">
+                      Login
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-    )
-    
+      </div>
+    );
   };
-
-
-
 
   return (
     <div className="home">
       <div className="home__container">
-        <img
-          className="home__image"
-          src={fitfit_background}
-        />
+        <img className="home__image" src={fitfit_background} />
 
         <div className="home__row">
           <div className="home__outerproduct">
@@ -158,7 +185,6 @@ function Home() {
               price={11.96}
               rating={5}
               image={fitness01}
-
               type="workout"
               difficulty={4}
               dailyDuration=" Avg. 38 Minutes -
@@ -168,9 +194,10 @@ function Home() {
               bodyFocus="Total Body"
               trainingType="Balance/Agility, Cardiovascular, HIIT, Pilates, Strength Training, Toning"
               equipment="Dumbbell"
-
             />
-            <button className="home__itembtn" onClick={handleClick}>Learn More</button>
+            <button className="home__itembtn" onClick={handleClick}>
+              Learn More
+            </button>
           </div>
           <div className="home__outerproduct">
             <Product
@@ -179,7 +206,6 @@ function Home() {
               price={239.0}
               rating={4}
               image={fitness02}
-
               type="workout"
               difficulty={3}
               dailyDuration="Avg. 34 Minutes -
@@ -190,7 +216,9 @@ function Home() {
               trainingType="Balance/Agility, Cardiovascular, Low Impact, Pilates, Strength Training, Toning, Stretching/Flexibility"
               equipment="Dumbbell"
             />
-            <button className="home__itembtn" onClick={handleClick}>Learn More</button>
+            <button className="home__itembtn" onClick={handleClick}>
+              Learn More
+            </button>
           </div>
         </div>
         <div className="home__row">
@@ -201,10 +229,10 @@ function Home() {
               price={199.99}
               rating={3}
               image={diet01}
-
-
             />
-            <button className="home__itembtn" onClick={handleClick}>Learn More</button>
+            <button className="home__itembtn" onClick={handleClick}>
+              Learn More
+            </button>
           </div>
           <div className="home__outerproduct">
             <Product
@@ -213,10 +241,10 @@ function Home() {
               price={98.99}
               rating={5}
               image={diet02}
-
-
             />
-            <button className="home__itembtn" onClick={handleClick}>Learn More</button>
+            <button className="home__itembtn" onClick={handleClick}>
+              Learn More
+            </button>
           </div>
           <div className="home__outerproduct">
             <Product
@@ -225,10 +253,10 @@ function Home() {
               price={598.99}
               rating={4}
               image={diet03}
-
-
             />
-            <button className="home__itembtn" onClick={handleClick}>Learn More</button>
+            <button className="home__itembtn" onClick={handleClick}>
+              Learn More
+            </button>
           </div>
         </div>
         <div className="home__row">
@@ -239,14 +267,14 @@ function Home() {
               price={1094.98}
               rating={3}
               image={diet04}
-
-
             />
-            <button className="home__itembtn" onClick={handleClick}>Learn More</button>
+            <button className="home__itembtn" onClick={handleClick}>
+              Learn More
+            </button>
           </div>
         </div>
         <div className="home__rowxd">
-        <div>{popupModal(0)}</div>
+          <div>{popupModal("49538094")}</div>
         </div>
       </div>
     </div>
--- src/UserProgress.js
+++ src/UserProgress.js
@@ -212,7 +212,7 @@ function UserProgress() {
                                 <Tooltip />
                                 <Legend />
                                 <Bar dataKey="pv" fill="#8884d8" background={{ fill: "#eee" }} />
-                                <Bar dataKey="uv" fill="#82ca9d" />
+                                <Bar dataKey="uv" fill="#82ca9d" background={{ fill: "#eee" }}/>
                             </BarChart>
 
                         </div>
